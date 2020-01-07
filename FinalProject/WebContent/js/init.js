a64ed8d3d80a266b76480146927fa9f9ad435040
User <User@eva666>
2020-01-07 23:28:16 +0800

    再見統計學

--- FinalProject/WebContent/js/init.js
+++ FinalProject/WebContent/js/init.js
@@ -1,5 +1,5 @@
 /*
-	Monochromed by TEMPLATED
+/	Monochromed by TEMPLATED
     templated.co @templatedco
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
